d69efc379820d52fb087ea5a3e86926dee8d5162
ppoyk <138587496+ppoyk@users.noreply.github.com>
2024-07-22 14:27:17 +0300

    streamline code

--- R/funs/pv_funktio.R
+++ R/funs/pv_funktio.R
@@ -23,47 +23,40 @@ pv_funktio <- function(m_id, a_id, period, ref_vuosi_vali, plot_dir) {
   pdata_m <- korkeus[korkeus$Paikka_Id == paikka_m$Paikka_Id, ]
   
   
-  seq_1 <- data.frame()
   # Aseta piirrettävän jakson aikaväli
   start <- as.POSIXct(period[[1]])
   # Aseta piirrettävän kuvaajan oikean reunan pvm
   end <- as.POSIXct(period[[2]])
+
+  
+  # Käsitellään manuaalimittaukset
+  pdata_m$vuosi <- data.table::year(pdata_m$Aika)
+  pdata_m$month <- data.table::month(pdata_m$Aika) |> factor(levels = c(1:12))
+  pdata_m$day <- data.table::mday(pdata_m$Aika)
   
   # Rajaa KAIKISTA man.mittauksista vertailujakso. Näytetään plottien taustalla
   pdata_m_ref <- subset(pdata_m,
                         data.table::year(pdata_m$Aika) >= ref_vuosi_vali[[1]] &
                           data.table::year(pdata_m$Aika) <= ref_vuosi_vali[[2]])
   
-  seq <- seq(from = start, by = 60*60*24, to = end)
-  day <- as.numeric(format(as.POSIXct(seq), format = "%d"))
-  month <- as.numeric(format(as.POSIXct(seq), format = "%m"))
-  year <- as.numeric(format(as.POSIXct(seq), format = "%Y"))
-  date <- format(as.POSIXct(seq), format = "%Y-%m-%d")
-  seq_1 <- cbind(seq, day, month, year, date)
+  # Rajaa manuaalimittaukset plottausvälille
+  pdata_m <- subset(pdata_m, pdata_m$Aika >= start & pdata_m$Aika <= end)
   
+  
+  #Rajaa automaatin data halutulle aikavälille (myös loppupäästä, yleensä ei päde)
+  pdata_a <- subset(pdata_a, pdata_a$Aika >= start & pdata_a$Aika <= end)
+  # TODO Tarvitaanko tätä ??
   pdata_a$vuosi <- data.table::year(pdata_a$Aika)
   pdata_a$month <- data.table::month(pdata_a$Aika)
   pdata_a$day <- data.table::mday(pdata_a$Aika)
-  pdata_a$pvm <- format(as.POSIXct(pdata_a$Aika, format = "%Y-%m-%d"), format = "%Y-%m-%d")
+  pdata_a$pvm <- format(as.POSIXct(pdata_a$Aika, format="%Y-%m-%d"), format="%Y-%m-%d")
   
-  pdata_a <- subset(pdata_a, pdata_a$pvm >= start)
-  
-  
-  pdata_m$vuosi <- data.table::year(pdata_m$Aika)
-  pdata_m$month <- data.table::month(pdata_m$Aika)
-  #pdata_m$day <- format(as.POSIXct(pdata_m$Aika, format = "%Y-%m-%d"), format = "%d")
-  pdata_m$day <- data.table::mday(pdata_m$Aika)
-  
-  # Ota manuaalimittauksista vertailujakso, joka näytetään plottien taustalla
-  pdata_m_ref <- subset(pdata_m,
-                        pdata_m$vuosi >= ref_vuosi_vali[[1]] &
-                          pdata_m$vuosi <= ref_vuosi_vali[[2]])
-  
+
   # Tarkista että löytyykö vertailujaksolta ollenkaan dataa
-  if (nrow(pdata_m_ref) >= 1) {
+  if (nrow(pdata_m_ref) > 0) {
     # Hae vielä vertailuvuosien jakso datasta plottauksen tekstejä varten
-    min_t <- as.numeric(min(pdata_m_ref$vuosi))
-    max_t <- as.numeric(max(pdata_m_ref$vuosi))
+    min_t <- min(pdata_m_ref$vuosi)
+    max_t <- max(pdata_m_ref$vuosi)
     vertailujakso_title <- paste("Vertailujakso",min_t,"-",max_t)
     
     pdata_m_ref$pvm <- format(as.POSIXct(pdata_m_ref$Aika), format = "%Y-%m-%d")
@@ -98,19 +91,14 @@ pv_funktio <- function(m_id, a_id, period, ref_vuosi_vali, plot_dir) {
   } else {
     vertailujakso_title <- paste("Ei vertailudataa jaksolta",
                                  ref_vuosi_vali[[1]],"-",ref_vuosi_vali[[2]])
-    df <- data.frame(matrix(NA, nrow=12, ncol=4))
-    colnames(df) <- c("kk", "mean", "min", "max")
+    ref_df <- data.frame(matrix(NA, nrow=12, ncol=4))
+    ref_df[,1] <- c(1:12)
+    colnames(ref_df) <- c("month", "ref_mean", "ref_min", "ref_max")
   }
   
   
   # Aineistojen yhdistys
-  yht <- base::merge(seq_1, pdata_a, by.x = "date", by.y = "pvm", all = TRUE)
-  yht_1 <- base::merge(yht, df, by.x = "month.x", by.y = "kk", all = TRUE)
-  yht_1 <- base::merge(yht_1, pdata_m, by.x = "Aika", by.y = "Aika", all.x = TRUE)
-  yht_1 <- base::merge(yht_1, paikka, by.x = "Paikka_Id.x", by.y = "Paikka_Id", all.x = TRUE)
-  
-  yht_1$Aika1 <- base::as.Date(as.POSIXct(yht_1$date, format = "%Y-%m-%d"))
-  
+  yht_1 <- base::merge(pdata_a, ref_df, by = "month", all = TRUE)
   
   # Aineistojen plottaus
   # Jos kuukausittaiset referenssiarvot on voitu laskea, piirrä ne
@@ -163,23 +151,24 @@ pv_funktio <- function(m_id, a_id, period, ref_vuosi_vali, plot_dir) {
   ggsave(tall_nimi_all,     plot, create.dir = T)
   
   yht_1 <- yht_1[!is.na(yht_1$Korkeus.x), ]
-  yht_1 <- yht_1[order(yht_1$date, decreasing = TRUE),]
+  yht_1 <- yht_1[order(yht_1$Aika, decreasing = TRUE),]
   
   tulostus <- c(
     "Tunnus_a" = paikka_a$Tunnus, "Tunnus_m" = paikka_m$Tunnus,
-    "Korkeus - ka" = yht_1[1, "Korkeus.x"] - yht_1[1, "mean"],
-    "Korkeus - min" = yht_1[1, "Korkeus.x"] - yht_1[1, "min"],
-    "Korkeus - max" = yht_1[1, "Korkeus.x"] - yht_1[1, "max"],
-    "Korkeus" = yht_1[1, "Korkeus.x"],
-    "Ka" = yht_1[1, "mean"], "Max" = yht_1[1, "max"],
-    "Min" = yht_1[1, "min"], "Pvm" = yht_1[1, "date"],
+    "asema_tunnus" = paikka_m$asema_tunnus, "asema" = paikka_m$asema_nimi,
+    "ref_ka_delta"  = yht_1[1, "Korkeus"] - yht_1[1, "ref_mean"],
+    "ref_min_delta" = yht_1[1, "Korkeus"] - yht_1[1, "ref_min"],
+    "ref_max_delta" = yht_1[1, "Korkeus"] - yht_1[1, "ref_max"],
+    "Korkeus" = yht_1[1, "Korkeus"],
+    "ref_ka" = yht_1[1, "ref_mean"], "ref_max" = yht_1[1, "ref_max"],
+    "ref_min" = yht_1[1, "ref_min"], "Pvm" = yht_1[1, "Aika"],
     "KoordErLat" = paikka_a$KoordErLat, "KoordErLong" = paikka_a$KoordErLong,
     "PohjavesiAlue_Id" = yht_1[1, "PohjavesiAlue_Id"],
     "pvalue_kokoluokka" = pval_kokolk)
   
   # Pyöristä numerosarakkeet (ei koordinaattisarakkeita)
-  for (s in names(koonti)[!names(koonti) %in% c("KoordErLat","KoordErLong")]) {
-    if (is.numeric(koonti[[s]])) koonti[[s]] <- round(koonti[[s]], 2)
+  for (s in names(tulostus)[!names(tulostus) %in% c("KoordErLat","KoordErLong")]) {
+    if (is.numeric(tulostus[[s]])) tulostus[[s]] <- round(tulostus[[s]], 2)
   }
   
   return(tulostus)
